36008be900f1c815d8ffa53633d9c3133f599a16
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-16 16:43:21 +0300

    html render

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  // res.status(500).json({ message: "Error" });
+  // res.json({ message: "Error" });
+  res.render("index", { text: "World" });
+});
+
 app.listen(3000);
