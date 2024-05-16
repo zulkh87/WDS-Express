4b8d6c48e70c47909310e1ea367ec07c201650e1
Zulkh87-AF-K <156850017+zulkh87@users.noreply.github.com>
2024-05-16 20:55:28 +0300

    routing refactor

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,40 @@ router.get("/new", (req, res) => {
   res.send("User New Form");
 });
 
+router.post("/", (req, res) => {
+  res.send("Create User");
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get User With ID ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`);
+  });
+
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
+
+// router.get("/:id", (req, res) => {
+//   res.send(`Get User With ID ${req.params.id}`);
+// });
+
+// router.put("/:id", (req, res) => {
+//   res.send(`Get User With ID ${req.params.id}`);
+// });
+
+// router.delete("/:id", (req, res) => {
+//   res.send(`Get User With ID ${req.params.id}`);
+// });
+
 module.exports = router;
